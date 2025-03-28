104e3ee1aad6c54acb3732581b39316c96c6135d
daniel-kav <dankavatha01@gmail.com>
2025-03-28 16:55:04 +0300

    Simplify DriverRegistrationSerializer by removing password confirmation field and related validation logic

--- Backend/tripwise/serializers.py
+++ Backend/tripwise/serializers.py
@@ -4,22 +4,33 @@ from django.contrib.auth.password_validation import validate_password
 
 User = get_user_model()
 
+# class DriverRegistrationSerializer(serializers.ModelSerializer):
+#     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+#     password2 = serializers.CharField(write_only=True, required=True)
+
+#     class Meta:
+#         model = User
+#         fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name', 
+#                  'phone_number', 'license_number', 'company_name')
+
+#     def validate(self, attrs):
+#         if attrs['password'] != attrs['password2']:
+#             raise serializers.ValidationError({"password": "Password fields didn't match."})
+#         return attrs
+
+#     def create(self, validated_data):
+#         validated_data.pop('password2')
+#         user = User.objects.create_user(**validated_data)
+#         return user
+
 class DriverRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name', 
-                 'phone_number', 'license_number', 'company_name')
-
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-        return attrs
+        fields = ('username', 'email', 'password')
 
     def create(self, validated_data):
-        validated_data.pop('password2')
         user = User.objects.create_user(**validated_data)
         return user
 
