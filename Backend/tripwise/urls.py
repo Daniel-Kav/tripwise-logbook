c759caeb8b81ef0e44271c46c83cf6a841d1b6c7
daniel-kav <dankavatha01@gmail.com>
2025-03-28 17:56:52 +0300

    get trips url

--- Backend/tripwise/urls.py
+++ Backend/tripwise/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import TripSavingView
+from .views import TripSavingView, UserTripsView
 
 urlpatterns = [
     path('auth/register/', views.DriverRegistrationView.as_view(), name='driver-register'),
     path('auth/login/', views.DriverLoginView.as_view(), name='driver-login'),
-    path('api/trip/save/', TripSavingView.as_view(), name='save_trip'),
+    path('trip/save/', TripSavingView.as_view(), name='save_trip'),
+    path('api/trip/user/<str:user_id>/', UserTripsView.as_view(), name='user_trips'),
 ]
