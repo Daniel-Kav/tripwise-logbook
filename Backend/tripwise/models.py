0063c5f117062d0c0812149f69a1060ae481b699
daniel-kav <dankavatha01@gmail.com>
2025-03-28 16:54:52 +0300

    Refactor Driver model to include username, email, and password fields

--- Backend/tripwise/models.py
+++ Backend/tripwise/models.py
@@ -4,9 +4,9 @@ from django.db import models
 # Create your models here.
 
 class Driver(AbstractUser):
-    phone_number = models.CharField(max_length=15, blank=True)
-    license_number = models.CharField(max_length=50, unique=True)
-    company_name = models.CharField(max_length=100, blank=True)
-    
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=50, unique=True)
+    password = models.CharField(max_length=50)
+
     def __str__(self):
         return self.username
