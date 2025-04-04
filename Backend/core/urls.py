30390eea06c05313853b9ebf2cc50d56eb0a5233
daniel-kav <dankavatha01@gmail.com>
2025-04-04 17:41:34 +0300

    home endpoint

--- Backend/core/urls.py
+++ Backend/core/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from tripwise.views import api_status
 
 urlpatterns = [
+    path('', api_status, name='api_status'),  # Root URL to show API status
     path('admin/', admin.site.urls),
     path('api/', include('tripwise.urls')),
 ]
