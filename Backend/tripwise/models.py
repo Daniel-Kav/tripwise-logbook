4f6cc91a90117e3cc7d082bf3a710c155acc437d
daniel-kav <dankavatha01@gmail.com>
2025-04-04 18:45:44 +0300

    model character len

--- Backend/tripwise/models.py
+++ Backend/tripwise/models.py
@@ -4,9 +4,9 @@ from django.db import models
 # Create your models here.
 
 class Driver(AbstractUser):
-    username = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(max_length=50, unique=True)
-    password = models.CharField(max_length=50)
+    username = models.CharField(max_length=250, unique=True)
+    email = models.EmailField(max_length=250, unique=True)
+    password = models.CharField(max_length=250)
 
     def __str__(self):
         return self.username
