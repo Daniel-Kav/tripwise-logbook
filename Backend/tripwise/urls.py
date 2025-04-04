cf519100b6fd74b9ddd516acd55491e49ca422ce
daniel-kav <dankavatha01@gmail.com>
2025-04-04 17:41:50 +0300

    endpoint config

--- Backend/tripwise/urls.py
+++ Backend/tripwise/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import TripSavingView, UserTripsView
+from .views import TripSavingView, UserTripsView, api_status
 
 urlpatterns = [
+    path('', api_status, name='api_root_status'),  # API root URL to show API status
     path('auth/register/', views.DriverRegistrationView.as_view(), name='driver-register'),
     path('auth/login/', views.DriverLoginView.as_view(), name='driver-login'),
     path('trip/save/', TripSavingView.as_view(), name='save_trip'),
